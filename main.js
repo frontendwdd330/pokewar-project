4e24118e323686b2c9af61b0e7f32ba95bb2090e
Eddy A. Renteria <90665395+co1424@users.noreply.github.com>
2023-03-25 09:49:05 -0600

    ER-BRANCH 3/25/2023

--- main.js
+++ main.js
@@ -13,6 +13,37 @@ When the page loads
 
 // we have to convert the response fetch sends back into the appropriate type. In this case the API sends back json...so we process it as json and send it back
 
+
+// Home Slideshow:
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+};
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+};
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 2500); // Change image every 2.5 seconds
+};
+
+
+
+
 function convertToJson(response) {
   if (response.ok) {
     return response.json();
