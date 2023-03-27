2aca5d536c4aec427dd9824cdd08bd1778cdb93f
Eddy A. Renteria <90665395+co1424@users.noreply.github.com>
2023-03-27 12:32:08 -0600

    SeparateJS-Files

--- slide-show.js
+++ slide-show.js
@@ -20,7 +20,7 @@ function showSlides() {
     if (slideIndex > slides.length) {
         slideIndex = 1
     }
-    slides[slideIndex - 1];
+    slides[slideIndex - 1].style.display = "block";
 
     setTimeout(showSlides, 2500); // Change image every 2.5 seconds
 };
